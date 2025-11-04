f4b496b1bd44cffbcf0b8e98d49d92cbdd7c0a35
SamarthArun12 <samarth.arun.ca@gmail.com>
2025-11-04 12:10:11 -0800

    Update WorkingClashing.py

--- WorkingClashing.py
+++ WorkingClashing.py
@@ -1,5 +1,6 @@
 import turtle as trtl
 import random
+import time
 from skill_class import Skill
 
 player_skill1 = Skill(4, 4, 3, "Player Skill 1")
@@ -12,20 +13,32 @@ def clashing(p_skill, e_skill):
     while clash:
         p_num = p_skill.calculate(p_coins)
         e_num = e_skill.calculate(e_coins)
+        print("Player rolls a " + str(p_num))
+        time.sleep(0.5)
+        print("Enemy rolls a " + str(e_num))
+        time.sleep(0.5)
         if p_num > e_num:
             e_coins -= 1
+            time.sleep(0.5)
+            print("Enemy loses 1 coin and has " + str(e_coins) + " remaining!")
         elif e_num > p_num:
             p_coins -= 1
+            time.sleep(0.5)
+            print("Player loses 1 coin and has" + str(p_coins) + " remaining!")
+        time.sleep(1)
+        print(" ")
         #nobody loses coins if roll same number
         if e_coins == 0:
             loser = "enemy" #placeholder, change later to enemy object
             damage_num = p_skill.calculate(p_coins)
             skill_name = p_skill.get_name()
+            print("Player attacks!")
             clash = False
         elif p_coins == 0:
             loser = "player" #placeholder, change later to player object
             damage_num = e_skill.calculate(e_coins)
             skill_name = e_skill.get_name()
+            print("Enemy attacks!")
             clash = False
     return loser, damage_num, skill_name
 
